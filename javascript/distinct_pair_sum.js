c54a7201d2bef5a0c4bcc63abeb71ed9733d2513
shu Lin <bbqlinshu@gmail.com>
2022-01-02 18:14:11 -0500

    Completed assignment

--- javascript/distinct_pair_sum.js
+++ javascript/distinct_pair_sum.js
@@ -1,5 +1,18 @@
 function distinctPairSum(arr, k) {
   // type your code here
+  let result = []
+  let temp = []
+  for (let i=0; i<arr.length; i++){
+    if(arr.indexOf(k-arr[i])!==-1 && arr.indexOf(k-arr[i])!==i && temp.indexOf(arr[i])===-1){
+      let group = []
+      group.push(arr[i])
+      group.push(arr[arr.indexOf(k-arr[i])])
+      temp.push(arr[i])
+      temp.push(arr[arr.indexOf(k-arr[i])])
+      result.push(group)
+    }
+  }
+  return result
 }
 
 if (require.main === module) {
@@ -11,6 +24,11 @@ if (require.main === module) {
 
   console.log("Expecting: [[2, 8]]");
   console.log("=>", distinctPairSum([3, 4, 2, 1, 5, 2, 8, 2], 10));
+
+  console.log("");
+
+  console.log("Expecting: []");
+  console.log("=>", distinctPairSum([3, 4, 2, 1, 5, 2], 10));
 }
 
 module.exports = distinctPairSum;
